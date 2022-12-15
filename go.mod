16ed0637403b9b63700bbcce1d5579a5bf8cd341
Jason Kincl <jkincl@redhat.com>
2022-12-15 15:28:08 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,95 @@
+module github.com/kincl/usbredir-hook
+
+go 1.18
+
+require (
+	github.com/google/gousb v1.1.2
+	github.com/spf13/pflag v1.0.5
+	google.golang.org/grpc v1.40.0
+	kubevirt.io/api v0.53.0
+	kubevirt.io/client-go v0.53.0
+	kubevirt.io/kubevirt v0.53.0
+	libvirt.org/go/libvirtxml v1.8009.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-kit/kit v0.9.0 // indirect
+	github.com/go-logfmt/logfmt v0.5.0 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/glog v1.0.0 // indirect
+	github.com/golang/mock v1.5.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/openshift/custom-resource-status v1.1.2 // indirect
+	github.com/pborman/uuid v1.2.0 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20210831024726-fe130286e0e2 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/api v0.23.5 // indirect
+	k8s.io/apiextensions-apiserver v0.23.1 // indirect
+	k8s.io/apimachinery v0.23.5 // indirect
+	k8s.io/client-go v12.0.0+incompatible // indirect
+	k8s.io/klog/v2 v2.40.1 // indirect
+	k8s.io/utils v0.0.0-20211116205334-6203023598ed // indirect
+	kubevirt.io/containerized-data-importer-api v1.47.0 // indirect
+	kubevirt.io/controller-lifecycle-operator-sdk v0.2.2 // indirect
+	sigs.k8s.io/json v0.0.0-20211020170558-c049b76a60c6 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
+)
+
+replace (
+	github.com/onsi/ginkgo/v2 => github.com/onsi/ginkgo/v2 v2.1.3
+	github.com/onsi/gomega => github.com/onsi/gomega v1.17.0
+	github.com/opencontainers/selinux => github.com/opencontainers/selinux v1.6.0
+	github.com/openshift/api => github.com/openshift/api v0.0.0-20191219222812-2987a591a72c
+	github.com/openshift/client-go => github.com/openshift/client-go v0.0.0-20210112165513-ebc401615f47
+	github.com/operator-framework/operator-lifecycle-manager => github.com/operator-framework/operator-lifecycle-manager v0.0.0-20190128024246-5eb7ae5bdb7a
+
+	k8s.io/api => k8s.io/api v0.23.1
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.23.1
+	k8s.io/apimachinery => k8s.io/apimachinery v0.23.1
+	k8s.io/apiserver => k8s.io/apiserver v0.23.1
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.23.1
+	k8s.io/client-go => k8s.io/client-go v0.23.1
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.23.1
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.23.1
+	k8s.io/code-generator => k8s.io/code-generator v0.23.1
+	k8s.io/component-base => k8s.io/component-base v0.23.1
+	k8s.io/cri-api => k8s.io/cri-api v0.23.1
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.23.1
+	k8s.io/klog => k8s.io/klog v0.4.0
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.23.1
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.23.1
+	k8s.io/kube-openapi => k8s.io/kube-openapi v0.0.0-20210113233702-8566a335510f
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.23.1
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.23.1
+	k8s.io/kubectl => k8s.io/kubectl v0.23.1
+	k8s.io/kubelet => k8s.io/kubelet v0.23.1
+	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.23.1
+	k8s.io/metrics => k8s.io/metrics v0.23.1
+	k8s.io/node-api => k8s.io/node-api v0.23.1
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.23.1
+	k8s.io/sample-cli-plugin => k8s.io/sample-cli-plugin v0.23.1
+	k8s.io/sample-controller => k8s.io/sample-controller v0.23.1
+
+	kubevirt.io/containerized-data-importer => kubevirt.io/containerized-data-importer v1.42.0
+	kubevirt.io/containerized-data-importer-api => kubevirt.io/containerized-data-importer-api v1.42.0
+
+	sigs.k8s.io/controller-runtime => sigs.k8s.io/controller-runtime v0.6.2
+)
---
+++ main.go
@@ -0,0 +1,249 @@
+/*
+ * This file is part of the KubeVirt project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2018 Red Hat, Inc.
+ *
+ */
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/spf13/pflag"
+	"google.golang.org/grpc"
+
+	vmSchema "kubevirt.io/api/core/v1"
+	"kubevirt.io/client-go/log"
+
+	"kubevirt.io/kubevirt/pkg/hooks"
+	hooksInfo "kubevirt.io/kubevirt/pkg/hooks/info"
+	hooksV1alpha1 "kubevirt.io/kubevirt/pkg/hooks/v1alpha1"
+	hooksV1alpha2 "kubevirt.io/kubevirt/pkg/hooks/v1alpha2"
+
+	"github.com/google/gousb"
+	"github.com/google/gousb/usbid"
+	"libvirt.org/go/libvirtxml"
+)
+
+const (
+	vendorProductAnnotation      = "usbredir.vm.kubevirt.io/vendorProduct"
+	onDefineDomainLoggingMessage = "Hook's OnDefineDomain callback method has been called"
+)
+
+type infoServer struct {
+	Version string
+}
+
+func (s infoServer) Info(ctx context.Context, params *hooksInfo.InfoParams) (*hooksInfo.InfoResult, error) {
+	log.Log.Info("Hook's Info method has been called")
+
+	return &hooksInfo.InfoResult{
+		Name: "smbios",
+		Versions: []string{
+			s.Version,
+		},
+		HookPoints: []*hooksInfo.HookPoint{
+			{
+				Name:     hooksInfo.OnDefineDomainHookPointName,
+				Priority: 0,
+			},
+		},
+	}, nil
+}
+
+type v1alpha1Server struct{}
+type v1alpha2Server struct{}
+
+func (s v1alpha2Server) OnDefineDomain(ctx context.Context, params *hooksV1alpha2.OnDefineDomainParams) (*hooksV1alpha2.OnDefineDomainResult, error) {
+	log.Log.Info(onDefineDomainLoggingMessage)
+	newDomainXML, err := onDefineDomain(params.GetVmi(), params.GetDomainXML())
+	if err != nil {
+		return nil, err
+	}
+	return &hooksV1alpha2.OnDefineDomainResult{
+		DomainXML: newDomainXML,
+	}, nil
+}
+func (s v1alpha2Server) PreCloudInitIso(_ context.Context, params *hooksV1alpha2.PreCloudInitIsoParams) (*hooksV1alpha2.PreCloudInitIsoResult, error) {
+	return &hooksV1alpha2.PreCloudInitIsoResult{
+		CloudInitData: params.GetCloudInitData(),
+	}, nil
+}
+
+func (s v1alpha1Server) OnDefineDomain(ctx context.Context, params *hooksV1alpha1.OnDefineDomainParams) (*hooksV1alpha1.OnDefineDomainResult, error) {
+	log.Log.Info(onDefineDomainLoggingMessage)
+	newDomainXML, err := onDefineDomain(params.GetVmi(), params.GetDomainXML())
+	if err != nil {
+		return nil, err
+	}
+	return &hooksV1alpha1.OnDefineDomainResult{
+		DomainXML: newDomainXML,
+	}, nil
+}
+
+func onDefineDomain(vmiJSON []byte, domainXML []byte) ([]byte, error) {
+	vmiSpec := vmSchema.VirtualMachineInstance{}
+	err := json.Unmarshal(vmiJSON, &vmiSpec)
+	if err != nil {
+		log.Log.Reason(err).Errorf("Failed to unmarshal given VMI spec: %s", vmiJSON)
+		panic(err)
+	}
+
+	annotations := vmiSpec.GetAnnotations()
+
+	if _, found := annotations[vendorProductAnnotation]; !found {
+		log.Log.Info("usbredir hook sidecar was requested, but no attributes provided. Returning original domain spec")
+		return domainXML, nil
+	}
+
+	vpid := strings.Split(annotations[vendorProductAnnotation], ":")
+	vidu64, _ := strconv.ParseUint(vpid[0], 16, 16)
+	vid := gousb.ID(uint16(vidu64))
+
+	pidu64, _ := strconv.ParseUint(vpid[1], 16, 16)
+	pid := gousb.ID(uint16(pidu64))
+
+	/*
+		...
+		<devices>
+		  <hostdev mode='subsystem' type='usb'>
+		    <source startupPolicy='optional' guestReset='off'>
+		      <vendor id='0x1234'/>
+		      <product id='0xbeef'/>
+		    </source>
+		    <boot order='2'/>
+		  </hostdev>
+		</devices>
+		...
+	*/
+
+	// domainSpec := libvirtxml.Domain{}
+	// err = xml.Unmarshal(domainXML, &domainSpec)
+	// if err != nil {
+	// 	log.Log.Reason(err).Errorf("Failed to unmarshal given domain spec: %s", domainXML)
+	// 	panic(err)
+	// }
+
+	// domainSpec := domainSchema.DomainSpec{}
+	// err = xml.Unmarshal(domainXML, &domainSpec)
+	// if err != nil {
+	// 	log.Log.Reason(err).Errorf("Failed to unmarshal given domain spec: %s", domainXML)
+	// 	panic(err)
+	// }
+
+	// hostDevice := domainSchema.HostDevice{
+	// 	Mode: "subsystem",
+	// 	Type: "usb",
+	// 	// <source/> does not have <vendor/> or <product/> defined
+	// 	Source: domainSchema.HostDeviceSource{
+	// 		Address: &domainSchema.Address{},
+	// 	},
+	// }
+	// domainSpec.Devices.HostDevices = append(domainSpec.Devices.HostDevices, hostDevice)
+
+	domainSpec := &libvirtxml.Domain{}
+	xml.Unmarshal([]byte(domainXML), &domainSpec)
+
+	// Initialize a new Context.
+	ctx := gousb.NewContext()
+	defer ctx.Close()
+
+	// Iterate through available Devices, finding all that match a known VID/PID.
+	// vid, pid := gousb.ID(0x1050), gousb.ID(0x0407)
+	devs, err := ctx.OpenDevices(func(desc *gousb.DeviceDesc) bool {
+		// this function is called for every device present.
+		// Returning true means the device should be opened.
+		return desc.Vendor == vid && desc.Product == pid
+	})
+	// All returned devices are now open and will need to be closed.
+	for _, d := range devs {
+		defer d.Close()
+	}
+	if err != nil {
+		log.Log.Errorf("OpenDevices(): %v", err)
+	}
+	if len(devs) == 0 {
+		log.Log.Errorf("no devices found matching VID %s and PID %s", vid, pid)
+	}
+
+	// Pick the first device found.
+	dev := devs[0]
+	log.Log.Infof("found %03d.%03d %s:%s %s\n", dev.Desc.Bus, dev.Desc.Address, dev.Desc.Vendor, dev.Desc.Product, usbid.Describe(dev.Desc))
+
+	bus := uint(dev.Desc.Bus)
+	device := uint(dev.Desc.Address)
+
+	usbDevice := libvirtxml.DomainHostdev{
+		SubsysUSB: &libvirtxml.DomainHostdevSubsysUSB{
+			Source: &libvirtxml.DomainHostdevSubsysUSBSource{
+				Address: &libvirtxml.DomainAddressUSB{
+					Bus:    &bus,
+					Device: &device,
+				},
+			},
+		},
+	}
+
+	hostdevs := domainSpec.Devices.Hostdevs
+	hostdevs = append(hostdevs, usbDevice)
+	domainSpec.Devices.Hostdevs = hostdevs
+
+	newDomainXML, err := xml.Marshal(domainSpec)
+	if err != nil {
+		log.Log.Reason(err).Errorf("Failed to marshal updated domain spec: %+v", domainSpec)
+		panic(err)
+	}
+
+	log.Log.Info("Successfully updated original domain spec with requested attributes")
+
+	return newDomainXML, nil
+}
+
+func main() {
+	log.InitializeLogging("usbredir-hook")
+
+	var version string
+	pflag.StringVar(&version, "version", "", "hook version to use")
+	pflag.Parse()
+
+	socketPath := filepath.Join(hooks.HookSocketsSharedDirectory, "smbios.sock")
+	socket, err := net.Listen("unix", socketPath)
+	if err != nil {
+		log.Log.Reason(err).Errorf("Failed to initialized socket on path: %s", socket)
+		log.Log.Error("Check whether given directory exists and socket name is not already taken by other file")
+		panic(err)
+	}
+	defer os.Remove(socketPath)
+
+	server := grpc.NewServer([]grpc.ServerOption{}...)
+
+	if version == "" {
+		panic(fmt.Errorf("usage: \n        /usbredir-hook --version v1alpha1|v1alpha2"))
+	}
+	hooksInfo.RegisterInfoServer(server, infoServer{Version: version})
+	hooksV1alpha1.RegisterCallbacksServer(server, v1alpha1Server{})
+	hooksV1alpha2.RegisterCallbacksServer(server, v1alpha2Server{})
+	log.Log.Infof("Starting hook server exposing 'info' and 'v1alpha1' services on socket %s", socketPath)
+	server.Serve(socket)
+}
